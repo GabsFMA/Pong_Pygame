fdb75dea483cd01fe5675e895baaa371ed24404d
Gabriel Francisco <105892017+GabsFMA@users.noreply.github.com>
2025-07-30 15:42:47 -0300

    feat: ball class

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 
-WIDHT = 800
+WIDTH = 800
 HEIGHT = 900
 
-screen = pygame.display.set_mode((WIDHT, HEIGHT))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("PONG")
 
 clock = pygame.time.Clock()
@@ -19,14 +19,14 @@ FPS = 30
 
 # Player class
 class Player:
-    def __init__(self, posx, posy, widht, height, speed, color):
+    def __init__(self, posx, posy, WIDTH, height, speed, color):
         self.posx = posx
         self.posy = posy
-        self.widht = widht
+        self.WIDTH = WIDTH
         self.height = height
         self.speed = speed
         self.color = color
-        self.geekRect = pygame.Rect(self.posx, self.posy, self.widht, self.height)
+        self.geekRect = pygame.Rect(self.posx, self.posy, self.WIDTH, self.height)
         self.geek = pygame.draw.rect(screen, self.color, self.geekRect)
 
     def display(self):
@@ -40,7 +40,7 @@ class Player:
         elif self.posy + self.height > HEIGHT:
             self.posy = HEIGHT - self.height
 
-        self.geekRect = pygame.Rect(self.posx, self.posy, self.widht, self.height)
+        self.geekRect = pygame.Rect(self.posx, self.posy, self.WIDTH, self.height)
 
     def displayScore(self, text, score, x, y, color):
         text = font.render(text+str(score), True, color)
@@ -52,5 +52,49 @@ class Player:
     def getRect(self):
         return self.geekRect
     
-    
+# Ball class
+class Ball:
+    def __init__(self, posx, posy, radius, speed, color):
+        self.posx = posx
+        self.posy = posy
+        self.radius = radius
+        self.speed = speed
+        self.color = color
+        self.xFac = 1
+        self.yFac = -1
+        self.ball = pygame.draw.circle(
+            screen, self.color, (self.posx, self.posy), self.radius)
+        self.firstTime = 1
+
+    def display(self):
+        self.ball = pygame.draw.circle(
+            screen, self.color, (self.posx, self.posy), self.radius)
+
+    def update(self):
+        self.posx += self.speed*self.xFac
+        self.posy += self.speed*self.yFac
+
+        if self.posy <= 0 or self.posy >= HEIGHT:
+            self.yFac *= -1
+
+        if self.posx <= 0 and self.firstTime:
+            self.firstTime = 0
+            return 1
+        elif self.posx >= WIDTH and self.firstTime:
+            self.firstTime = 0
+            return -1
+        else:
+            return 0
+
+    def reset(self):
+        self.posx = WIDTH//2
+        self.posy = HEIGHT//2
+        self.xFac *= -1
+        self.firstTime = 1
+
+    def hit(self):
+        self.xFac *= -1
+
+    def getRect(self):
+        return self.ball
             
