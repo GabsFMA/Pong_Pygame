8623e49f70d0e74575b1cfcb9bfbe6a5514086e0
Gabriel Francisco <105892017+GabsFMA@users.noreply.github.com>
2025-07-30 15:37:55 -0300

    feat: player class

--- main.py
+++ main.py
@@ -16,3 +16,41 @@ pygame.display.set_caption("PONG")
 
 clock = pygame.time.Clock()
 FPS = 30
+
+# Player class
+class Player:
+    def __init__(self, posx, posy, widht, height, speed, color):
+        self.posx = posx
+        self.posy = posy
+        self.widht = widht
+        self.height = height
+        self.speed = speed
+        self.color = color
+        self.geekRect = pygame.Rect(self.posx, self.posy, self.widht, self.height)
+        self.geek = pygame.draw.rect(screen, self.color, self.geekRect)
+
+    def display(self):
+        self.geek = pygame.draw.rect(screen, self.color, self.geekRect)
+
+    def update(self, yFac):
+        self.posy = self.posy + self.speed * yFac
+
+        if self.posy < 0:
+            self.posy = 0
+        elif self.posy + self.height > HEIGHT:
+            self.posy = HEIGHT - self.height
+
+        self.geekRect = pygame.Rect(self.posx, self.posy, self.widht, self.height)
+
+    def displayScore(self, text, score, x, y, color):
+        text = font.render(text+str(score), True, color)
+        textRect = text.get_rect()
+        textRect.center = (x, y)
+
+        screen.blit(text, textRect)
+    
+    def getRect(self):
+        return self.geekRect
+    
+    
+            
